06d35b25ca55af7f7d404f9600f02459a6cfb10b
Choo Hyun Ho <hyunho4178@gmail.com>
2023-04-14 02:28:43 +0900

    일단 해보자
    일단 해보자

---
+++ ChitChat/Modules/Landing/Builder.swift
@@ -0,0 +1,15 @@
+//
+//  Builder.swift
+//  Landing
+//
+//  Created by 추현호 on 2023/04/14.
+//
+
+import UIKit
+
+public final class Builder {
+    
+    public static func build() -> UIViewController {
+        return UIViewController()
+    }
+}
